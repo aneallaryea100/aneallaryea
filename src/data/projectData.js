1e2db059af3115edd13cc4e065b61fce964d4276
Aneal Laryea <aneallaryea100@gmail.com>
2022-12-31 15:38:18 +0000

    update project section

---
+++ src/data/projectData.js
@@ -0,0 +1,31 @@
+const projectData = [  
+    {
+        id: 1,
+        name: 'TAL-ART CONFERENCE',
+        description: 'The webpage displays information about venues, speakers and events of a conference. The speaker section of the webpage is built dynamically with JavaScript. ',
+        image: 'https://media.licdn.com/dms/image/C4E2DAQFVS9kSRVnvgA/profile-treasury-image-shrink_800_800/0/1662128987970?e=1673096400&v=beta&t=IAJaK0tx011c3JH-HewAolGKSq58D6P8UzwtRHl6qRY',
+        livedemo: 'https://aneallaryea100.github.io/Capstone/#',
+        github: 'https://github.com/aneallaryea100/Capstone/tree/capstone',
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        id: 2,
+        name: 'Solovino Tv Show',
+        description: 'This web application allows the user to like and comment on a movie. Movies displayed on the UI are being fetched from an API.',
+        image: 'https://media.licdn.com/dms/image/C4E2DAQGGhR5EhkQeeg/profile-treasury-image-shrink_800_800/0/1662484653790?e=1673100000&v=beta&t=c1Xy7p6njMjbSRksFDRppvpW5RcxQ9WV8Uu2-uFknfg',
+        livedemo: 'https://diegomero.github.io/javascript-capstone/dist/',
+        github: 'https://github.com/DiegoMero/javascript-capstone',
+        technologies: ['HTML', 'CSS', 'JavaScript']
+    },
+    {
+        id: 3,
+        name: 'Rick and Morty',
+        description: 'Rick and Morty is a American adult animated science-fiction sitcom. This web application fetches data from the rick and morty API and allows user to know more about each character in the sitcom.',
+        image: 'https://github.com/aneallaryea100/Rick_Morty/raw/testing/src/images/ricky.png',
+        livedemo: 'https://diegomero.github.io/javascript-capstone/dist/',
+        github: 'https://github.com/DiegoMero/javascript-capstone',
+        technologies: ['React', 'Redux', 'Jest']
+    }, 
+]
+
+export default projectData;
