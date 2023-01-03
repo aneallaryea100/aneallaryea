f837a378e3bd89296c76a6d4999b8d1068512bd5
Aneal Laryea <aneallaryea100@gmail.com>
2023-01-03 23:48:57 +0000

    Update welcome page

--- src/component/welcome/welcome.jsx
+++ src/component/welcome/welcome.jsx
@@ -5,7 +5,7 @@ export default function Welcome() {
   return (
     <div className='welcomeContainer' id='welcome'>
       <div className='aneal'>
-      <h1 className='nametext'><code>Hello <span class="wave">👋</span>,  I'm Aneal Laryea.</code></h1>
+      <h1 className='nametext'><code>Hello <span class="wave">👋</span>,<br className='breakName'/>  I'm Aneal Laryea.</code></h1>
       <p className='paragraphtext'>I'm a full-stack web developer.</p>
       <button className='talkbtn' type='button'><code><a href="mailto:aneallaryea100@gmail.com" target="_blank" rel="noreferrer" className='talklink'>Let talk</a></code></button>
       </div>
